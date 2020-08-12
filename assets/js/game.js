f00846ea42cb4869be9e9c1eec6696fb714804b1
adams-ears <adamsears146@gmail.com>
2020-08-12 19:58:36 +0000

    game variables and reset values in place

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,15 +1,42 @@
+/* Game Variables */
+var thegame = {
+    playorder: [],
+    comporder: [],
+    runorder: [],
+    count: 0
+};
 
-let command = ['uarrow','rarrow','larrow','darrow','space'];
-let score = 0;
-let localStorageName = "keybScore";
-
-
-/* choose a random command from the command array */
-function chooseRandomCommand(a) {
-    return commands[Math.floor(Math.random() * commands.length)];
-}
 
 /* Start Game */
   $("#start").click(function () {
         newGame();
     });
+
+//Resets all the values, calls for function compgo//
+    function newGame() {
+     game.comporder = [];
+     game.playorder = [];
+     game.runorder = [];
+     game.count = 0;
+        compgo();
+    }
+
+
+
+
+
+
+
+
+
+   /* Icon functions */
+    function firstbutton() {
+        var audio = document.getElementById("arrowu");
+        audio.play();
+        $(".arrowup").addClass("effect-logo");
+        setTimeout(function () {
+            $(".arrowup").css("color", "");
+            $(".fas").removeClass("effect-logo");
+        }, 600);
+    }
+
