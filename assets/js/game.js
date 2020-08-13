1d513f0f6c6b8642429bef0a539e311be3f9f8de
adams-ears <adamsears146@gmail.com>
2020-08-13 14:33:31 +0000

    Game Variables in place

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,42 +1,7 @@
-/* Game Variables */
-var thegame = {
-    playorder: [],
-    comporder: [],
-    runorder: [],
-    count: 0
-};
+/* game variables */
 
-
-/* Start Game */
-  $("#start").click(function () {
-        newGame();
-    });
-
-//Resets all the values, calls for function compgo//
-    function newGame() {
-     game.comporder = [];
-     game.playorder = [];
-     game.runorder = [];
-     game.count = 0;
-        compgo();
-    }
-
-
-
-
-
-
-
-
-
-   /* Icon functions */
-    function firstbutton() {
-        var audio = document.getElementById("arrowu");
-        audio.play();
-        $(".arrowup").addClass("effect-logo");
-        setTimeout(function () {
-            $(".arrowup").css("color", "");
-            $(".fas").removeClass("effect-logo");
-        }, 600);
-    }
-
+let gameorder = ["arrowup", "quaver", "arrowrgt", "arrowleft", "arrowdown"];
+let computerturn = [];
+let playerturn = [];
+let score = 0;
+let highscore;
