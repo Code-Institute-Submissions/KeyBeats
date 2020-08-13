a120123f19c1ae058251c3b0bb744c65f3718059
adams-ears <adamsears146@gmail.com>
2020-08-13 15:37:19 +0000

    new game variables added

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,3 +5,18 @@ let computerturn = [];
 let playerturn = [];
 let score = 0;
 let highscore;
+
+
+/* Random sequence for icon selection  */
+function sequence(gameorder){  
+return gameorder[Math.floor(Math.random()*gameorder.length)];
+}
+
+/* Start new game */
+    $("#start").click(function () {
+        newGame();
+    });
+
+function newGame(){
+     $("#start").addClass("hide-button");
+}
