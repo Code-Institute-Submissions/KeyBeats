27805c0cb4195f8272c95ff67f02bff9a79d91a1
adams-ears <adamsears146@gmail.com>
2020-08-16 15:23:55 +0000

    trying to make random sequence work with icon glowing

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,16 +1,14 @@
 /* game variables */
+var game =
+ gameorder =[];
+ computerturn = [];
+ playerturn = [];
+ score = 0;
 
-let gameorder = ["arrowup", "quaver", "arrowrgt", "arrowleft", "arrowdown"];
-let computerturn = [];
-let playerturn = [];
-let score = 0;
-let highscore;
 
 
 /* Random sequence for icon selection  */
-function sequence(gameorder){  
-return gameorder[Math.floor(Math.random()*gameorder.length)];
-}
+
 
 /* Start new game */
     $("#start").click(function () {
@@ -19,4 +17,51 @@ return gameorder[Math.floor(Math.random()*gameorder.length)];
 
 function newGame(){
      $("#start").addClass("hide-button");
+     game.gameorder = [];
+     game.computerturn = [];
+     game.playerturn = [];
+     game.score = 0;
+     sequence();
 }
+
+
+ function sequence() {
+    
+        setTimeout(function () {
+            for (i = 0; i < game.score; i++) {
+                game.gameorder.push(Math.floor(Math.random() * 5));
+            }
+            for (i = 0; i < game.gameorder.length; i++) {
+                iconGlow(i);
+            }
+        }, 1000);
+    } 
+
+function iconGlow(i){
+        setTimeout(function () {
+            if (game.gameorder[i] == 0) {
+                game.computerturn.push(i);
+                iconOne();
+            }
+      if (game.computerturn.length == game.gameorder.length) {
+                setTimeout(function () {
+                    playerTurn();
+                }, 200);
+            }
+        }, 1200 * i);
+    }
+
+
+
+      function iconOne() {
+        var audio = document.getElementById("audioarrowu");
+        audio.play();
+        $(".arrowup").addClass("effect-logo");
+
+        setTimeout(function () {
+            $(".fas").removeClass("effect-logo");
+        }, 600);
+    }
+
+
+   
