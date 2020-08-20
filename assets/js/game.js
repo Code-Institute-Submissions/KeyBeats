be3569fdd810e191be39c841031e234aad817a8b
adams-ears <adamsears146@gmail.com>
2020-08-20 19:30:42 +0000

    score function implmeneted , if else compare sequence statement going to retry function correctly

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,7 +5,10 @@ let game = {
   playerturn: [],
   score: 0,
   count: 0
+
 };
+/* Score and highscore */
+let yourscore = 0;
 
 /* Start new game */
 $("#start").click(function () {
@@ -18,12 +21,8 @@ function newGame() {
   game.computerturn = [];
   game.playerturn = [];
   game.score = 9999;
-  game.count = 0
-  $(".arrowup").disabled = false;
-       $(".quaver").disabled = false;
-       $(".arrowrgt").disabled = false;
-       $(".arrowleft").disabled = false;
-       $(".arrowdown").disabled = false;
+/*   game.count = 0 */
+  resetScore(); 
   sequence();
 }
 
@@ -31,14 +30,14 @@ function newGame() {
 function sequence() {
   setTimeout(function () {
     for (i = 0; i < game.score; i++) {
-      game.gameorder.push(Math.floor(Math.random() * 5));
+      game.gameorder.push(Math.floor(Math.random() ,100* 5));
     }
     for (i = 0; i < game.gameorder.length; i++) {
       iconGlow(i);
     }
   }, 1000);
 }
-/* Delay between sequence using settimeout. Advice from mentor to look for loop delay so all lights dont flash together https://www.geeksforgeeks.org/how-to-add-a-delay-in-a-javascript-loop/ */
+/* Delay between sequence using set timeout. Advice from mentor to look for loop delay so all lights dont flash together https://www.geeksforgeeks.org/how-to-add-a-delay-in-a-javascript-loop/ */
 function iconGlow(i) {
   setTimeout(function () {
     if (game.gameorder[i] == 0) {
@@ -74,10 +73,12 @@ function iconGlow(i) {
     if (game.gameorder.length == game.computerturn.length) {
       setTimeout(function () {
         playerturn();
-      }, 200);
+      }, 600);
     }
   }, 1200* i);
 }
+
+
 /* Players Turn */
 function playerturn() {
   $(".game-icon").on("click", function () {
@@ -92,50 +93,44 @@ function playerturn() {
 $("#arrowups").click(function () {
   game.playerturn.push(0);
   logoOnea();
-});
-$("#arrowups").keypress(function (e) {
-  game.playerturn.push(0);
-  logoOnea();
+  compareSequence();
 });
 
 $("#notes").click(function () {
   game.playerturn.push(1);
   logoTwoa();
+    compareSequence(); 
 });
 $("#arrowr").click(function () {
   game.playerturn.push(2);
   logoThreea();
+    compareSequence(); 
 });
 $("#arrowl").click(function () {
   game.playerturn.push(3);
   logoFoura();
+    compareSequence();
 });
 $("#arrowd").click(function () {
   game.playerturn.push(4);
   logoFivea();
+  compareSequence();
 });
+
 
 /* function for if player fails */
     function compareSequence() {
-        if (game.playerSequence[game.count] != game.computerSequence[game.count]) {
-            $("#start").click(function () {
-                retry();
-            });
-                game.count = 0;
-            } else {
-                playerTurn();
-                game.count++;
-            }
+        if (game.playerturn[game.playerturn.length -1] == game.computerturn[game.computerturn.length -1]) {
+                /* updateScoreBoard(); */ console.log("right")
+        }else{
+            /* retry(); */ console.log("wrong")
         }
-    
+    }
+        
+
 /* Add Retry button on failure, disables buttons to stop cheating*/
          function retry() {
        $("#start").addClass("show-button");
-       $(".arrowup").disabled = true;
-       $(".quaver").disabled = true;
-       $(".arrowrgt").disabled = true;
-       $(".arrowleft").disabled = true;
-       $(".arrowdown").disabled = true;
          }
 
 
@@ -210,4 +205,19 @@ function logoFivea() {
   setTimeout(function () {
     $(".fas").removeClass("effect-logoclick");
   }, 700);
+}
+
+
+/* Scoreboard and highscore */
+function updateScoreBoard() {
+    yourscore++;
+    let scoreBox = document.getElementById("score");
+    scoreBox.textContent = yourscore;
+    
+}
+
+function resetScore() {
+    let yourscore = 0;
+    let scoreBox = document.getElementById("score");
+    scoreBox.textContent = score;
 }
